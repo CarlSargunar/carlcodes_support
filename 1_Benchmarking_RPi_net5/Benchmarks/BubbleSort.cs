78eee22efb3ba0e5aa61750688f285b99acf88d1
Carl Sargunar <carl@sargunar.com>
2021-02-17 15:07:34 +0000

    bla

---
+++ 1_Benchmarking_RPi_net5/Benchmarks/BubbleSort.cs
@@ -0,0 +1,22 @@
+namespace Benchmark.Benchmarks
+{
+    public class BubbleSort
+    {
+        public int[] Sort(int[] input)
+        {
+            if (input == null || input.Length == 0)
+            {
+                return null;
+            }
+
+            int[] output = new int[input.Length]();
+
+
+
+            return output;
+
+        }
+
+
+    }
+}
